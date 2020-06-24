32d2051f3fb0f9822c6477ea58bbb57dfaeb1a9d
Gabi Nagy <gabrian.nagy@gmail.com>
2020-06-24 09:58:16 +0300

    Add Measure, Nutrient models to "nutrients" app

--- nutrients/admin.py
+++ nutrients/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Measure, Nutrient
+
+
+admin.site.register(Measure)
+admin.site.register(Nutrient)
--- nutrients/models.py
+++ nutrients/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Measure(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    # e.g. unit: "gram", symbol: "g"
+    unit = models.CharField(max_length=16, unique=True)
+    symbol = models.CharField(max_length=8, unique=True)
+
+    # This should only be needed if we need to support multiple units of measurement
+    # for the same quantity i.e. support both "gram" and "kilogram". If this is the case,
+    # we should know that they measure they same quantity and, provided the right formulas,
+    # may be added together if needed.
+    # QUANTITY_CHOICES = [
+    #     (CALORIE, 'calorie'),
+    #     (WEIGHT, 'weight'),
+    #     (VOLUME, 'volume'),
+    # ]
+    # quantity = models.CharField(max_length=8, choices=QUANTITY_CHOICES)
+
+    def __str__(self):
+        return self.unit
+
+
+class Nutrient(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    # e.g. "Calories" or "Protein"
+    name = models.CharField(max_length=16, unique=True)
+
+    # models.PROTECT: prevent deletion of the referenced object by raising ProtectedError
+    measure = models.ForeignKey(Measure, related_name='nutrients', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
