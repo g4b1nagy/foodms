67d759ff4403cf9e3612d8919b30937b577eae71
Gabi Nagy <gabrian.nagy@gmail.com>
2020-06-24 11:08:07 +0300

    Show nutrients in Food admin

--- foods/admin.py
+++ foods/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 from .models import Food, FoodNutrient
 
 
-admin.site.register(Food)
+class FoodNutrientInline(admin.TabularInline):
+    model = FoodNutrient
+    extra = 1
+
+
+class FoodAdmin(admin.ModelAdmin):
+    inlines = (FoodNutrientInline,)
+
+
+admin.site.register(Food, FoodAdmin)
 admin.site.register(FoodNutrient)
